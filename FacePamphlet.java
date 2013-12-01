d16cf8c995ca0ec88eed0f961f1184ecc21a81f0
S <s>
2013-12-01 11:05:09 -0800

    {"type":"TI", "runs":"1"}

--- FacePamphlet.java
+++ FacePamphlet.java
@@ -61,15 +61,15 @@ public class FacePamphlet extends ConsoleProgram implements FacePamphletConstant
 			println("Lookup: " + nameField.getText());
 			nameField.setText("");
 		}
-		if(e.getSource() == statusField || e.getActionCommand() == "Change Status" && !statusField.getText().isEmpty()) {
+		if(e.getSource().equals(statusField) || e.getActionCommand() == "Change Status" && !statusField.getText().isEmpty()) {
 			println("Change Status: " + statusField.getText());
 			statusField.setText("");
 		}
-		if(e.getSource() == pictureField || e.getActionCommand() == "Change Picture" && !pictureField.getText().isEmpty()) {
+		if(e.getSource().equals(pictureField) || e.getActionCommand() == "Change Picture" && !pictureField.getText().isEmpty()) {
 			println("Change Picture: " + pictureField.getText());
 			pictureField.setText("");
 		}
-		if(e.getSource() == friendField || e.getActionCommand() == "Add Friend" && !friendField.getText().isEmpty()) {
+		if(e.getSource().equals(friendField) || e.getActionCommand() == "Add Friend" && !friendField.getText().isEmpty()) {
 			println("Add Friend: " + friendField.getText());
 			friendField.setText("");
 		}
