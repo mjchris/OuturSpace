a2169942145443530a0ade21c5cbcfdfa17a5175
S <s>
2013-12-01 11:49:38 -0800

    {"type":"TI", "runs":"0"}

--- FacePamphletProfile.java
+++ FacePamphletProfile.java
@@ -93,7 +93,7 @@ public class FacePamphletProfile implements FacePamphletConstants {
 		boolean result;
 		if(friends != null) {
 			result = !friends.contains(friend);
-			if(friends.)
+			if(result)
 			friends.add(friend);
 		} else {
 			friendsMap.put(name, friends);
