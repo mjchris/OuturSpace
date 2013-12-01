f445bb41f0233c35b550cd62dcbd2439068415ad
S <s>
2013-12-01 12:45:01 -0800

    {"type":"TI", "runs":"0"}

--- FacePamphletProfile.java
+++ FacePamphletProfile.java
@@ -78,8 +78,9 @@ public class FacePamphletProfile implements FacePamphletConstants {
 /* Method: setStatus(status) */
 /** This method sets the status associated with the profile. */ 
 	public void setStatus(String status) {
-		statusMap.remove(name);
-		statusMap.put(name, status);
+		
+/**		statusMap.remove(name);
+		statusMap.put(name, status); */
 	}
 
 /* Method: addFriend(friend) */
