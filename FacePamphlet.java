63cbac2b1f513466843cf784069b8a274ebcfbfe
S <s>
2013-12-01 12:12:17 -0800

    {"type":"TI", "runs":"0"}

--- FacePamphlet.java
+++ FacePamphlet.java
@@ -80,6 +80,7 @@ public class FacePamphlet extends ConsoleProgram implements FacePamphletConstant
 		}
 		if((e.getSource().equals(friendField) || e.getActionCommand() == "Add Friend") && !friendField.getText().isEmpty()) {
 			profile.addFriend(friendField.getText());
+			println(profile.toString());
 			friendField.setText("");
 		}
 		if(e.getActionCommand() == "Remove Friend" && !friendField.getText().isEmpty()) {
