f62d81c08142e3af95bd2f52514fa81074d47181
S <s>
2013-12-01 08:22:22 -0800

    {"type":"TI", "runs":"0"}

--- FacePamphlet.java
+++ FacePamphlet.java
@@ -26,6 +26,7 @@ public class FacePamphlet extends Program implements FacePamphletConstants {
  * initialization that needs to be performed.
  */
 	public void init() {
+		add(new JLabel("Name"), NORTH);
 		
     }
     
