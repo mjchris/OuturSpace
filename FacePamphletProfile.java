6381caa1f8e007f6f47a4849f74a8a10b355587c
S <s>
2013-12-01 11:34:06 -0800

    {"type":"TI", "runs":"0"}

--- FacePamphletProfile.java
+++ FacePamphletProfile.java
@@ -125,7 +125,7 @@ public class FacePamphletProfile implements FacePamphletConstants {
  * friends in this profile.
  */ 
 	public String toString() {
-		String toString = name;
+		String toString = name + " (";
 		return toString;
 	}
 	
