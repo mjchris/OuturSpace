233228bba79a59999e9ddb707a4bf9994cee041f
S <s>
2013-12-02 10:46:25 -0800

    {"type":"TI", "runs":"0"}

--- FacePamphlet.java
+++ FacePamphlet.java
@@ -82,6 +82,7 @@ public class FacePamphlet extends ConsoleProgram implements FacePamphletConstant
 				println("Delete: profile for " + nameField.getText() + " does not exist");
 			}
 			currentProfile = null;
+			println("--> Current profile: " + currentProfile.toString());
 			nameField.setText("");
 		}
 		if(e.getActionCommand() == "Lookup" && !nameField.getText().isEmpty()) {
