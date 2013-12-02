e1cdb005e051082e5ba958cfcf6dd3e92e546e94
S <s>
2013-12-02 10:43:40 -0800

    {"type":"TI", "runs":"0"}

--- FacePamphlet.java
+++ FacePamphlet.java
@@ -129,7 +129,7 @@ public class FacePamphlet extends ConsoleProgram implements FacePamphletConstant
 					if(!friendCheck.contains(friendField.getText())) {
 						currentProfile.addFriend(friendField.getText());
 						database.addProfile(currentProfile);
-						database.addProfile(database.getProfile(friendField.getText()))
+						database.addProfile(database.getProfile(friendField.getText()).addFriend(currentProfile.getName()));
 						println("Add Friend: " + friendField.getText() + ": " + currentProfile.toString());
 					} else {
 						println(currentProfile.getName() + " already has that friend");
