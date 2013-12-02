3e7603299821689f110cfba3e89b46523454f9d4
S <s>
2013-12-02 10:35:28 -0800

    {"type":"TI", "runs":"0"}

--- FacePamphlet.java
+++ FacePamphlet.java
@@ -121,9 +121,7 @@ public class FacePamphlet extends ConsoleProgram implements FacePamphletConstant
 			if(currentProfile != null) {
 				if(database.containsProfile(friendField.getText())) {
 					while(currentProfile.getFriends().hasNext()) {
-						if(currentProfile.getFriends().next() == friendField.getText()) {
-							println(currentProfile.getName() + " already has that friend.");
-						}
+						
 					}
 				}
 			} else {
