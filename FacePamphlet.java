6c38bc461924090a436d39783258d16bbc75a7e6
S <s>
2013-12-02 14:44:45 -0800

    {"type":"TI", "runs":"1"}

--- FacePamphlet.java
+++ FacePamphlet.java
@@ -137,6 +137,7 @@ public class FacePamphlet extends ConsoleProgram implements FacePamphletConstant
 			pictureField.setText("");
 		}
 		if(e.getActionCommand() == "Add Friend" && !friendField.getText().isEmpty()) {
+			
 			ArrayList<String> friendCheck = new ArrayList<String>();
 			if(currentProfile != null) {
 				if(database.containsProfile(friendField.getText())) {
