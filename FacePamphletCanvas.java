06b4ee7a525aba55bab08ffa6e9523f480404413
S <s>
2013-12-02 15:02:51 -0800

    {"type":"TI", "runs":"0"}

--- FacePamphletCanvas.java
+++ FacePamphletCanvas.java
@@ -33,7 +33,7 @@ public class FacePamphletCanvas extends GCanvas implements FacePamphletConstants
  */
 	public void showMessage(String msg) {
 		remove(this.msg);
-		msg = new GLabel("")
+		msg = new GLabel();
 	}
 	
 /* Method displayProfile(profile) */
