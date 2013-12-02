6063bf7411f39200675ba1081471ab82084f8017
S <s>
2013-12-02 14:50:34 -0800

    {"type":"TI", "runs":"0"}

--- FacePamphlet.java
+++ FacePamphlet.java
@@ -139,27 +139,23 @@ public class FacePamphlet extends ConsoleProgram implements FacePamphletConstant
 		if(e.getActionCommand() == "Add Friend" && !friendField.getText().isEmpty()) {
 			ArrayList<String> friendCheck = new ArrayList<String>();
 			if(currentProfile != null) {
-				if(friendField.getText() != currentProfile.getName()) {
-					println("Users cannot be friends with themselves.");
-				} else {
-					if(database.containsProfile(friendField.getText())) {
-						Iterator<String> friendsIterator = currentProfile.getFriends();
-						while(friendsIterator.hasNext()) {
-							friendCheck.add(friendsIterator.next());
-						}
-						if(!friendCheck.contains(friendField.getText())) {
-							currentProfile.addFriend(friendField.getText());
-							database.addProfile(currentProfile);
-							profile = database.getProfile(friendField.getText());
-							profile.addFriend(currentProfile.getName());
-							database.addProfile(profile);
-							println("Add Friend: " + friendField.getText() + ": " + currentProfile.toString());
-						} else {
-							println(currentProfile.getName() + " already has that friend.");
-						}
-					} else {
-						println("That profile does not exist.");
+				if(database.containsProfile(friendField.getText())) {
+					Iterator<String> friendsIterator = currentProfile.getFriends();
+					while(friendsIterator.hasNext()) {
+						friendCheck.add(friendsIterator.next());
 					}
+					if(!friendCheck.contains(friendField.getText())) {
+						currentProfile.addFriend(friendField.getText());
+						database.addProfile(currentProfile);
+						profile = database.getProfile(friendField.getText());
+						profile.addFriend(currentProfile.getName());
+						database.addProfile(profile);
+						println("Add Friend: " + friendField.getText() + ": " + currentProfile.toString());
+					} else {
+						println(currentProfile.getName() + " already has that friend.");
+					}
+				} else {
+					println("That profile does not exist.");
 				}
 			} else {
 				println("Select a profile first.");
