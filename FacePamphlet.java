1ff06dc6ca3236b9bbfe870e8a8cc5167ad3eb0c
S <s>
2013-12-03 00:42:10 -0800

    {"type":"TI", "runs":"0"}

--- FacePamphlet.java
+++ FacePamphlet.java
@@ -217,6 +217,7 @@ public class FacePamphlet extends Program implements FacePamphletConstants {
 				}
 			} else {
 				msg = "Please select a profile to remove friend";
+				canvas.removeAll();
 			}
 			if(currentProfile == null) {
 				canvas.removeAll();
