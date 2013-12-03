39fd00c649d053ed130af7b52ec38366563579a1
S <s>
2013-12-02 19:50:35 -0800

    {"type":"TI", "runs":"0"}

--- FacePamphletCanvas.java
+++ FacePamphletCanvas.java
@@ -76,7 +76,8 @@ public class FacePamphletCanvas extends GCanvas implements FacePamphletConstants
 		}
 		if(profile.getStatus() != null) {
 			GLabel status = new GLabel(profile.getStatus());
-			status.setFont()
+			status.setFont(PROFILE_STATUS_FONT);
+			
 		}
 	}
 
