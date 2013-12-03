2ad4fbf42eba16c69f6d64e99eb917a97c50b879
S <s>
2013-12-02 19:09:53 -0800

    {"type":"TI", "runs":"0"}

--- FacePamphletCanvas.java
+++ FacePamphletCanvas.java
@@ -55,7 +55,7 @@ public class FacePamphletCanvas extends GCanvas implements FacePamphletConstants
 	
 /* Method addName(profile) */
 /**
- * 
+ * This method adds the name of the current profile to the canvas.
  */
 
 }
