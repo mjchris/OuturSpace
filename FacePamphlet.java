f163d8a38e4f5eb276068561dfd0f5865f0af758
S <s>
2013-12-03 13:31:32 -0800

    {"type":"TI", "runs":"0"}

--- FacePamphlet.java
+++ FacePamphlet.java
@@ -27,6 +27,7 @@ public class FacePamphlet extends Program implements FacePamphletConstants {
 
 /* Instance variable for JTextFields */
 	private JTextField nameField = new JTextField(TEXT_FIELD_SIZE);
+	private JTextField fileField = new JTextField(TEXT_FIELD_SIZE);
 	private JTextField statusField = new JTextField(TEXT_FIELD_SIZE);
 	private JTextField pictureField = new JTextField(TEXT_FIELD_SIZE);
 	private JTextField friendField = new JTextField(TEXT_FIELD_SIZE);
