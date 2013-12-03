b5eaa16819fdc718ef57320f24209586c322e687
S <s>
2013-12-02 23:16:56 -0800

    {"type":"TI", "runs":"0"}

--- FacePamphlet.java
+++ FacePamphlet.java
@@ -117,7 +117,7 @@ public class FacePamphlet extends Program implements FacePamphletConstants {
 			if(currentProfile != null) {
 				currentProfile.setStatus(statusField.getText());
 				database.addProfile(currentProfile);
-				println("Change Status: " + currentProfile.toString());
+				
 			} else {
 				msg = "Please select a profile to change status";
 			}
