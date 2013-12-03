de923b56b8ae32b93cdc2fabed3502884c2bc2bd
S <s>
2013-12-02 22:58:55 -0800

    {"type":"TI", "runs":"0"}

--- FacePamphletCanvas.java
+++ FacePamphletCanvas.java
@@ -72,7 +72,7 @@ public class FacePamphletCanvas extends GCanvas implements FacePamphletConstants
 			status.setLocation(LEFT_MARGIN, TOP_MARGIN + name.getAscent() + IMAGE_MARGIN + IMAGE_HEIGHT + STATUS_MARGIN + status.getAscent());
 			add(status);
 		} else {
-			status 
+			status = new GLabel("No current status");
 		}
 		GLabel friendLabel = new GLabel("Friends:");
 		friendLabel.setFont(PROFILE_FRIEND_LABEL_FONT);
