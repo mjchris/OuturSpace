d8db2a234f2cdb96891bf3a187d23ef70279f16c
S <s>
2013-12-02 19:37:48 -0800

    {"type":"TI", "runs":"0"}

--- FacePamphletCanvas.java
+++ FacePamphletCanvas.java
@@ -70,6 +70,8 @@ public class FacePamphletCanvas extends GCanvas implements FacePamphletConstants
 		} else {
 			add(new GRect(LEFT_MARGIN, name.getY() + IMAGE_MARGIN, IMAGE_WIDTH, IMAGE_HEIGHT));
 			GLabel noImage = new GLabel("No Image");
+			noImage.setFont(PROFILE_IMAGE_FONT);
+			noImage.setLocation()
 		}
 	}
 
