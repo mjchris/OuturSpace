d4289a06a32a79ead21830b822938ad891a02d05
S <s>
2013-12-03 00:14:53 -0800

    {"type":"TI", "runs":"0"}

--- FacePamphlet.java
+++ FacePamphlet.java
@@ -147,6 +147,7 @@ public class FacePamphlet extends Program implements FacePamphletConstants {
 			} else {
 				canvas.displayProfile(currentProfile);
 			}
+			canvas.showMessage(msg);
 			pictureField.setText("");
 		}
 		if(e.getActionCommand() == "Add Friend" && !friendField.getText().isEmpty()) {
