c73aecbc2c77df9c685ce72615595b07af58c578
S <s>
2013-12-03 00:40:27 -0800

    {"type":"TI", "runs":"0"}

--- FacePamphlet.java
+++ FacePamphlet.java
@@ -144,11 +144,6 @@ public class FacePamphlet extends Program implements FacePamphletConstants {
 				msg = "Please select a profile to change picture";
 				canvas.removeAll();
 			}
-			if(currentProfile == null) {
-				canvas.removeAll();
-			} else {
-				canvas.displayProfile(currentProfile);
-			}
 			canvas.showMessage(msg);
 			pictureField.setText("");
 		}
